bb236b9cdb19430376bc0c6a86fa22bb8a6f2e20
sebasped <sebasped@disroot.org>
2019-01-29 20:50:58 -0300

    ex1

---
+++ machine-learning-ex1/ex1/sebas.m
@@ -0,0 +1,7 @@
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+% Plot Data
+% Note: You have to complete the code in plotData.m
+plotData(X, y);
